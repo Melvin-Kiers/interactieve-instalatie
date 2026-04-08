b2103f5e1d56c47368f33cc8fcd7b015a2143a02
Melvin-Kiers <melvinkiers2003@gmail.com>
2026-04-08 14:57:49 +0200

    Update

--- src/components/NameInput.js
+++ src/components/NameInput.js
@@ -11,16 +11,21 @@ export default function NameInput({ setUsername }) {
   };
 
   return (
-    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", marginTop: 50 }}>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-        onKeyDown={handleKeyDown}
-        placeholder="Vul je naam in en druk Enter"
-        style={{ padding: 10, fontSize: 16 }}
-      />
-      <p style={{ marginTop: 10 }}>Druk op Enter om verder te gaan</p>
+    <div className="start">
+      <div className="text">
+        <h1>Dit is de tekst die boven staat</h1>
+      </div>
+      <div className="input_field" style={{ display: "flex", flexDirection: "column", alignItems: "center", marginTop: 50 }}>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          onKeyDown={handleKeyDown}
+          placeholder="Vul je naam in en druk Enter"
+          style={{ padding: 10, fontSize: 16 }}
+        />
+        <p style={{ marginTop: 10 }}>Druk op Enter om verder te gaan</p>
+      </div>
     </div>
   );
 }
